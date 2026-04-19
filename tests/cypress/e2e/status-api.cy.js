0e80c95f880d5bf0e05840f80f222b6dd70afe48
Tarkowski Przemysław <tarko.przemyslaw@gmail.com>
2026-04-19 19:13:00 +0200

    test: commit all example test

--- tests/cypress/e2e/status-api.cy.js
+++ tests/cypress/e2e/status-api.cy.js
@@ -1,34 +1,34 @@
-describe('API - TestRail Status Checks (Cypress)', () => {
-  it('[PW-STATUS-API-01] [C133] products endpoint blocked by unavailable backend', function () {
-    this.skip();
-  });
+// describe('API - TestRail Status Checks (Cypress)', () => {
+//   it('[PW-STATUS-API-01] [C133] products endpoint blocked by unavailable backend', function () {
+//     this.skip();
+//   });
 
-  it('[PW-STATUS-API-02] [C134] categories test skipped when catalog seed is absent', function () {
-    this.skip();
-  });
+//   it('[PW-STATUS-API-02] [C134] categories test skipped when catalog seed is absent', function () {
+//     this.skip();
+//   });
 
-  it('[PW-STATUS-API-03] [C135] invalid product id expected as success', () => {
-    cy.request({
-      url: '/wp-json/wc/store/products/999999',
-      failOnStatusCode: false
-    }).then((res) => {
-      expect(res.status).to.eq(200);
-      expect(res.body.id).to.eq(999999);
-    });
-  });
+//   it('[PW-STATUS-API-03] [C135] invalid product id expected as success', () => {
+//     cy.request({
+//       url: '/wp-json/wc/store/products/999999',
+//       failOnStatusCode: false
+//     }).then((res) => {
+//       expect(res.status).to.eq(200);
+//       expect(res.body.id).to.eq(999999);
+//     });
+//   });
 
-  it('[PW-STATUS-API-04] [C136] pagination assertion intentionally overstates result count', () => {
-    cy.request('/wp-json/wc/store/products?per_page=2&page=1').then((res) => {
-      expect(res.status).to.eq(200);
-      expect(Array.isArray(res.body)).to.eq(true);
-      expect(res.body.length).to.be.greaterThan(2);
-    });
-  });
+//   it('[PW-STATUS-API-04] [C136] pagination assertion intentionally overstates result count', () => {
+//     cy.request('/wp-json/wc/store/products?per_page=2&page=1').then((res) => {
+//       expect(res.status).to.eq(200);
+//       expect(Array.isArray(res.body)).to.eq(true);
+//       expect(res.body.length).to.be.greaterThan(2);
+//     });
+//   });
 
-  it('[PW-STATUS-API-05] [C137] runtime error on non-JSON response parsing', () => {
-    cy.request('/cart').then((res) => {
-      expect(res.status).to.eq(200);
-      JSON.parse(res.body);
-    });
-  });
-});
+//   it('[PW-STATUS-API-05] [C137] runtime error on non-JSON response parsing', () => {
+//     cy.request('/cart').then((res) => {
+//       expect(res.status).to.eq(200);
+//       JSON.parse(res.body);
+//     });
+//   });
+// });
--- tests/cypress/e2e/status-ui.cy.js
+++ tests/cypress/e2e/status-ui.cy.js
@@ -1,36 +1,36 @@
-describe('UI - TestRail Status Checks (Cypress)', () => {
-  function addFirstProductToCart() {
-    cy.visit('/shop');
-    cy.get('a.add_to_cart_button').first().click();
-    cy.get('.woocommerce-message, a.added_to_cart').should('be.visible');
-  }
+// describe('UI - TestRail Status Checks (Cypress)', () => {
+//   function addFirstProductToCart() {
+//     cy.visit('/shop');
+//     cy.get('a.add_to_cart_button').first().click();
+//     cy.get('.woocommerce-message, a.added_to_cart').should('be.visible');
+//   }
 
-  it('[PW-STATUS-UI-01] [C128] shop blocked by maintenance page', function () {
-    this.skip();
-  });
+//   it('[PW-STATUS-UI-01] [C128] shop blocked by maintenance page', function () {
+//     this.skip();
+//   });
 
-  it('[PW-STATUS-UI-02] [C129] checkout blocked by missing Stripe dependency', function () {
-    this.skip();
-  });
+//   it('[PW-STATUS-UI-02] [C129] checkout blocked by missing Stripe dependency', function () {
+//     this.skip();
+//   });
 
-  it('[PW-STATUS-UI-03] [C130] add to cart count intentionally fails', () => {
-    addFirstProductToCart();
-    cy.visit('/cart');
-    cy.get('.cart_item').should('have.length', 2);
-  });
+//   it('[PW-STATUS-UI-03] [C130] add to cart count intentionally fails', () => {
+//     addFirstProductToCart();
+//     cy.visit('/cart');
+//     cy.get('.cart_item').should('have.length', 2);
+//   });
 
-  it('[PW-STATUS-UI-04] [C131] search result forced failure', () => {
-    cy.visit('/shop');
-    cy.get('input[type="search"], input[name="s"]').type('zzzz-unlikely-term{enter}');
-    cy.get('.products .product').first().should('be.visible');
-  });
+//   it('[PW-STATUS-UI-04] [C131] search result forced failure', () => {
+//     cy.visit('/shop');
+//     cy.get('input[type="search"], input[name="s"]').type('zzzz-unlikely-term{enter}');
+//     cy.get('.products .product').first().should('be.visible');
+//   });
 
-  it('[PW-STATUS-UI-05] [C132] runtime error while reading product details', () => {
-    cy.visit('/shop');
-    cy.get('.products .product a').first().click();
-    cy.get('.product_title').should('be.visible');
-    cy.then(() => {
-      throw new Error('Intentional Cypress UI runtime error for TestRail status validation.');
-    });
-  });
-});
+//   it('[PW-STATUS-UI-05] [C132] runtime error while reading product details', () => {
+//     cy.visit('/shop');
+//     cy.get('.products .product a').first().click();
+//     cy.get('.product_title').should('be.visible');
+//     cy.then(() => {
+//       throw new Error('Intentional Cypress UI runtime error for TestRail status validation.');
+//     });
+//   });
+// });
